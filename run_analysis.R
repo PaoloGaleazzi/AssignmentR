8770f9ab5b1621fab81d63a0208bb1c781cc4cef
Paolo Galeazzi <48617432+PaoloGaleazzi@users.noreply.github.com>
2019-05-25 21:58:54 +0200

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,58 @@
+install.packages("dplyr")
+library(dplyr)
+install.packages("downloader")
+library(downloader)
+file.create("data.zip")
+zipurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download(zipurl, dest="data.zip", mode="wb") 
+unzip ("data.zip", exdir = "./")
+wd <- getwd()
+mainpath <- file.path(wd, "UCI HAR Dataset")
+trainpath <- file.path(mainpath, "train")
+testpath <- file.path(mainpath, "test")
+setwd(testpath)
+test <- tbl_df(read.table("X_test.txt"))
+testact <- tbl_df(read.table("y_test.txt"))
+testsubj <- tbl_df(read.table("subject_test.txt"))
+setwd(trainpath)
+train <- tbl_df(read.table("X_train.txt"))
+trainact <- tbl_df(read.table("y_train.txt"))
+trainsubj <- tbl_df(read.table("subject_train.txt"))
+setwd(mainpath)
+subjects <- rbind(trainsubj, testsubj)
+acts <- rbind(trainact, testact)
+varnames <- read.table("features.txt")
+varnames <- as.character(varnames$V2)
+df <- rbind(train, test)
+colnames(df) <- varnames
+meanstd <- grep("mean\\(\\)|std\\(\\)", names(df))
+df <- df[,meanstd]
+df <- tbl_df(cbind(acts,df))
+df <- rename(df, "activity" = V1)
+df$activity[df$activity==1] <- "WALKING"
+df$activity[df$activity==2] <- "WALKING_UPSTAIRS"
+df$activity[df$activity==3] <- "WALKING_DOWNSTAIRS"
+df$activity[df$activity==4] <- "SITTING"
+df$activity[df$activity==5] <- "STANDING"
+df$activity[df$activity==6] <- "LAYING"
+df <- tbl_df(cbind(subjects, df))
+df <- rename(df, "subjects" = V1)
+dfby <- group_by(df, activity, subjects) %>% summarize_all(mean)
+df
+dfby
+View(df)
+View(dfby)
+
+setwd(wd)
+pathdf <- file.path(wd, "df.txt")
+pathdfby <- file.path(wd, "dfby.txt")
+write.table(df, pathdf, row.names=FALSE)
+write.table(dfby, pathdfby, row.names=FALSE)
+
+file.remove("data.zip")
+unlink("UCI HAR Dataset", recursive = TRUE)
+# detach("package:downloader", unload=TRUE)
+# detach("package:dplyr", unload=TRUE)
+# remove.packages("downloader")
+# remove.packages("dplyr")
+
